3bc65e04c03f1f70b091dda705a7e4e0a06049db
Carlos Pasten <carlos92.pastenbravo@gmail.com>
2021-12-13 16:17:42 -0300

    re-render en title

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@
 
 import fetchData from './utils/fetchData.js';
 const search = document.querySelector('.search');
-
+const title = document.querySelector('.title');
 
 // inicializacion de la pagina y renderizando en el DOM los componentes dinamicos
 fetchData.getProducts(fetchData.API_URL, HTMLResponse);
@@ -16,9 +16,14 @@ renderCategories();
 // la funcion showEveryone a diferencia de hacer un re-render manda una solicitud otra vez a la API
 // para que se muestren todos los productos y luego hace un render de los componenetes quitando cualquier
 // filtro que se haya aplicado
-showEveryone.onclick = () => {
+
+const reRender = () =>{
     search.value = '';
     resetPagination();
     fetchData.getProducts(fetchData.API_URL, HTMLResponse);
     fetchData.getCategories(fetchData.API_URL);
 }
+
+title.onclick  = () => reRender();
+showEveryone.onclick = () => reRender();
+
